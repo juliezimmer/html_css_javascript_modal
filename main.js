bf63f0b8536f30f09c6cc68a0f1654a78c0dc1fd
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2017-12-28 19:18:37 -0600

    added modal content

--- main.js
+++ main.js
@@ -12,7 +12,12 @@ var closeBtn = document.getElementsByClassName('closeBtn')[0];
 modalBtn.addEventListener('click', openModal);
 
 //Use eventListener to listen for clos click
-closeBtn.addEventListener('click', closeModal)
+closeBtn.addEventListener('click', closeModal);
+
+//Add eventListener for clicks outside the modal
+window.addEventListener('click', outsideClick); 
+
+
 
 //Function to open the modal
 /* This function has to change the display property with value none to display: block.  */
@@ -24,4 +29,13 @@ function openModal () {
 function closeModal () {
   //chnge the display value to none
   modal.style.display = 'none';
+}
+
+//function that runs when clicking outside modal in window
+//the target should be equal to modal 
+//e is an event parameter
+function outsideClick (e) {
+  if(e.target == modal) {
+      modal.style.display = 'none';
+  }
 }
